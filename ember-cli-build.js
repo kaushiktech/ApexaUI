c00d5d47e6973b09020dc4e53938bcb613f0a477
Kaushik Kundu <kaushik-tech@hotmail.com>
2024-12-15 07:10:25 -0500

    Initial commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
-
+    sassOptions: {
+      includePaths: ['node_modules/bootstrap/scss'],
+    },
     // Add options here
   });
-
+  app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
   return app.toTree();
 };
